894d106d4d9b843c427f41637f4b4e5103034f41
VictorvanDen <qwe1234567890qweqwe@yandex.ru>
2023-05-03 20:37:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using ConsoleApp5;
+//using System; дз: сделать два класса: паспорт и человек, у человека может быть "свой паспорт"
+                //в своем паспорте объект "человек" "может увидеть все данные", в чужом нет
+                //в своем паспорте человек может изменить имя, но оно не должно содержать число 1
+                //задача реализовать это.
+
+//pelmeshka PlusP(pelmeshka a1, pelmeshka a2)
+//{
+//    a1.dolya_myasa += a2.dolya_myasa;
+//    a1.kg += a2.kg;
+//    a1.name += a2.name;
+//    return a1;
+//}
+//void PrintP(pelmeshka a1)
+//{
+//    Console.WriteLine(a1.dolya_myasa+ " " + a1.kg+ " " + a1.name);
+//}
+
+Console.WriteLine("Hello, World!");
+pelmeshka P1 = new pelmeshka(1, 2, "u");
+pelmeshka P2 = new pelmeshka(1, 2, "y");
+double o = P1.getKg();
+P1.Plus(P2);
+Console.WriteLine(P1.Print());
+//P1 = P1 + P2;
+//P1 = PlusP(P1, P2);
+//PrintP(P1);
+//List<int> o= new List<int>();
+//o.Add(1);
---
+++ ConsoleApp5/pelmeshka.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class pelmeshka
+    {
+        double dolya_myasa;
+        double kg;
+        public double getKg()
+        {
+            return this.kg;
+        }
+        public void setKg(double kgU)
+        {
+            if(kgU < 0)
+            {
+            
+            }
+            else
+            {
+                this.kg = kgU;
+            }
+        }
+        string name;
+        public pelmeshka(double dolya_myasa, double kg, string name)
+        {
+            this.dolya_myasa = dolya_myasa;
+            this.kg = kg;
+            this.name = name;
+        }
+        public void Plus(pelmeshka Arg)
+        {
+            this.kg += Arg.kg;
+            this.dolya_myasa += Arg.dolya_myasa;
+            this.name += Arg.name;
+        }
+        public string Print()
+        {
+            return this.name + " " + this.kg + " " + this.dolya_myasa;
+        }
+    }
+}
